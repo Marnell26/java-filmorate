222348ef84e54facc9885de5bfbf1be2878bba85
Marnell26 <Marnell26@yandex.ru>
2025-08-29 09:30:17 +0300

    Исправления по ФЗ 10. Создан новый класс-глобальный обработчик ошибок, создан класс ErrorResponse для сообщений об ошибках, создана кастомная аннотация для валидации даты релиза, переделаны тесты.

--- src/main/java/ru/yandex/practicum/filmorate/validation/OldReleaseDate.java
+++ src/main/java/ru/yandex/practicum/filmorate/validation/OldReleaseDate.java
@@ -1,20 +1,23 @@
 package ru.yandex.practicum.filmorate.validation;
 
 import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
-import jakarta.validation.Payload;
 
 import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-@Target({ FIELD } )
+@Target({FIELD})
 @Retention(RUNTIME)
 @Constraint(validatedBy = ReleaseDateValidator.class)
 @Documented
 public @interface OldReleaseDate {
     String message() default "Дата релиза должна быть не раньше 28.12.1895";
+
     Class<?>[] groups() default {};
+
     Class<? extends Payload>[] payload() default {};
 }
